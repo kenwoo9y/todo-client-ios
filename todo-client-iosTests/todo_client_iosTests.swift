4a9d1399f2d9b35cc3cd90c027683689b8679b2f
kenwoo9y <kenwoo9y@gmail.com>
2025-05-05 10:44:19 +0900

    create project

---
+++ todo-client-iosTests/todo_client_iosTests.swift
@@ -0,0 +1,10 @@
+import Testing
+@testable import todo_client_ios
+
+struct todo_client_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
