dbaf69e86cf06cf11638c263bfee9f763d27b546
kenwoo9y <kenwoo9y@gmail.com>
2025-05-06 18:02:17 +0900

    add TaskTableView

--- todo-client-ios/Views/HomeView.swift
+++ todo-client-ios/Views/HomeView.swift
@@ -9,38 +9,7 @@ struct HomeView: View {
             ZStack {
                 // メインコンテンツ
                 CommonLayout(isShowingSideMenu: $sideMenuViewModel.isShowingSideMenu) {
-                    VStack(spacing: 0) {
-                        // ヘッダー
-                        HStack {
-                            Text("タイトル")
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                            Text("期日")
-                                .frame(width: 100)
-                            Text("ステータス")
-                                .frame(width: 80)
-                        }
-                        .padding()
-                        .background(Color.gray.opacity(0.1))
-                        
-                        // タスク一覧
-                        ScrollView {
-                            LazyVStack(spacing: 0) {
-                                ForEach(taskListViewModel.tasks) { task in
-                                    HStack {
-                                        Text(task.title)
-                                            .frame(maxWidth: .infinity, alignment: .leading)
-                                        Text(task.dueDate)
-                                            .frame(width: 100)
-                                        Text(task.status.rawValue)
-                                            .frame(width: 80)
-                                    }
-                                    .padding()
-                                    .background(Color.white)
-                                    Divider()
-                                }
-                            }
-                        }
-                    }
+                    TaskTableView(taskListViewModel: taskListViewModel)
                 }
                 
                 // サイドメニュー
